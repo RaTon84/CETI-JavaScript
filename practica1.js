f97e0bbd689dd26324aca3933a3d22bc5e3146ff
Hernan <hjviltez@gmail.com>
2024-04-22 12:50:48 -0300

    hasta practica 28

--- practica1.js
+++ practica1.js
@@ -68,6 +68,113 @@ function largest(a,b,c){
 largest(1,2,3)
 /*13*/
 
+/*14*/
+function pathJoin(base,folder,os){
+    if(os==="windows") {
+        return base +"\\"+folder 
+    }else {
+        return base+"/"+folder
+    } 
+}
+pathJoin("hola","path","windows")
+/*15*/
+"hola".length //tamaño del string
+"hola".charAt(0) //indice
+/*16-17*/
+function printLetters(word){
+    for (let index = 0; index < word.length; index++) {
+        console.log(word.charAt(index))        
+    }
+}
+printLetters("hola")
+/*18*/
+"1"==1 // true
+"1"===1 //false (agrega la comparacion del tipo de dato)
+/*19*/
+function arrayFromTo(start,end){
+    for (let index=start; index<=end; index++) {
+        console.log(index)        
+    }    
+}
+arrayFromTo(3,7)
+/*20*/
+function arrayFromToStepping(start,end,step){
+    for (let index=start; index<=end; index+=step) {
+        console.log(index)        
+    }    
+}
+arrayFromToStepping(3,13,3)
+/*21*/
+let numeros = [3,9,4,7,1]
+function maxIn(array){
+    var numMax= array[0]
+    array.forEach(element => {
+        if(element>numMax){
+            numMax=element
+        }
+    });
+    return numMax
+}
+maxIn(numeros)
+/*22*/
+var arr=[1,2,3]
+arr.push(4)
+console.log(arr)
+/*23*/
+let alCuadrado = [2,3,4]
+function squared(array){
+    var arrSquared=[]
+    array.forEach(element => {
+        arrSquared.push(Math.pow(element,2))
+    });
+    return arrSquared
+}
+squared(alCuadrado)
+/*24*/
+let even = [2,3,4,8,3,4,3,2,2]
+function evens(array){
+    var arrPares=[]
+    array.forEach(element => {
+        if (element%2==0) {
+            arrPares.push(element)
+        }        
+    });
+    return arrPares
+}
+evens(even)
+/*25*/
+let numbers2 = [2,3,4,8,3]
+function reversed(array){
+    var arrReversed=[]
+    for (let index = array.length; index > 0; index--) {
+        arrReversed.push(numbers2[index-1])               
+    }
+    return arrReversed
+}
+reversed(numbers2)
+/*26*/
+function ladderPattern(height){
+    var returnh = ""
+    for (let index = 0; index < height; index++) {        
+        returnh = returnh + "X"
+        console.log(returnh)        
+    }
+}
+ladderPattern(5)
+/*27*/
+function invertedLadderPattern(height){
+    var returnh = ""
+    for (let index = 0; index < height; index++) {        
+        returnh = returnh + "X"        
+    }
+    for (let index=height; index>0; index--) {
+        console.log(returnh.substr(0,index))        
+    }
+}
+invertedLadderPattern(5)
+/*28*/
+
+
 /*entrega 1
 Se pide que escriba la función personasConIniciales(personas, iniciales)
 que dado un array de personas y un String que representa iniciales 
