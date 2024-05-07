42e69330828d52c04d9763dc692a8cbe84428183
Hernan <hjviltez@gmail.com>
2024-05-07 10:13:15 -0300

    Create nodeServer.js

---
+++ practicas en Vsc/server/nodeServer.js
@@ -0,0 +1,24 @@
+/*// server.mjs
+import { createServer } from 'node:http';
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+// run with `node server.mjs`*/
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
