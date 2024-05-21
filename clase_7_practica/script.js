c82dbaf675bb6f7a48139abb0863c9cba0ffe8b6
Hernan <hjviltez@gmail.com>
2024-05-20 21:47:30 -0300

    205

---
+++ clase_7_practica/script.js
@@ -0,0 +1,48 @@
+
+const btnPop = document.getElementById('btn_pop');
+
+document.addEventListener("DOMContentLoaded", function(){
+    const p = document.getElementById('parrafo');
+    p.addEventListener('mouseout', function(){
+        p.style.backgroundColor="green";
+        p.innerHTML="Estoy fuera";
+    });
+    p.addEventListener('mouseover', function(){
+        p.style.backgroundColor='blue';
+        p.innerText="Entre";
+    });
+    /////////////////////////////////////////////////////
+
+    const btnPop = document.getElementById('btn_pop');
+    const popup = document.getElementById('pop_up');
+    const btnClosed = document.getElementById('closed');
+    const pop_menu = document.getElementById('pop_menu')
+    //const winClosed = document.body
+
+    btnPop.addEventListener('click', function(){
+        popup.style.display="block";
+        //windowsClosed();          
+    })
+
+    btnClosed.addEventListener('click', function(){
+        popup.style.display="none";
+    })
+
+    popup.addEventListener('click', function(event){
+            if(event.target!=pop_menu)
+            popup.style.display="none";        
+    });
+
+    /*
+    function windowsClosed(){
+        winClosed.addEventListener('click', function(){
+            alert('fef');
+           // popup.style.display="none";
+        });
+    }*/
+    
+});
+
+
+
+
